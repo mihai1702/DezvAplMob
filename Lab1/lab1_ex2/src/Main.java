9d1516bdd6c53d83d0caaf653761e2a90cf3fd36
mihai1702 <apopi.mihai@gmail.com>
2025-02-18 17:03:31 +0200

    update

--- Lab1/lab1_ex2/src/Main.java
+++ Lab1/lab1_ex2/src/Main.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
+    public static void friendNumbs(int a, int b){
         int sumA=1;
         int sumB=1;
         for(int i=2;i<=a/2;i++)
@@ -22,6 +19,12 @@ public class Main {
         else System.out.println("False");
         System.out.println("Sum of the first number is: "+sumA);
         System.out.println("Sum of the second number is: "+sumB);
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        friendNumbs(a,b);
     }
 }
