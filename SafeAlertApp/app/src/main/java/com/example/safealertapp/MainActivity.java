7dff33ab7b49f83fd6dbc76be8169ff86ab83610
mihai1702 <apopi.mihai@gmail.com>
2025-02-19 23:08:08 +0200

    SafeAlertApp
    started the app development, added functionality for sending message and location to favorite contacts from agenda

---
+++ SafeAlertApp/app/src/main/java/com/example/safealertapp/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.safealertapp;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.Manifest;
+import android.location.Location;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int SMS_PERMISSION_CODE = 1;
+    private static final int LOCATION_PERMISSION_CODE = 2;
+    private FusedLocationProviderClient fusedLocationClient;
+//    List<emergContact>emergContacts=new ArrayList<>(Arrays.asList(
+//            new emergContact("Oana", "0748231348"),
+//            new emergContact("Mihai", "0787596450"),
+//            new emergContact("Razvan", "0748562056"),
+//            new emergContact("Bogdan", "0755285622")
+//    ));
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        List<emergContact>emergContacts=getFavoriteContacts(this);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        ///permisiuni SMS
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_CODE);
+        }
+        ///perimisiuni locatie
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+        }
+        ///permisiuni contacte
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+        }
+
+        Button EmergContact = findViewById(R.id.EmergButton);
+
+        EmergContact.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                for (emergContact contact : emergContacts) {
+                    sendLocationAndMessage(contact);
+                }
+            }
+        });
+    }
+
+    public List<emergContact> getFavoriteContacts(Context context) {
+        List<emergContact> Contacts = new ArrayList<>();
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+            return Contacts; // Returnează listă goală până la obținerea permisiunii
+        }
+
+        ContentResolver contentResolver = context.getContentResolver();
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+
+        String selection = ContactsContract.Contacts.STARRED + "=?";
+        String[] selectionArgs = new String[]{"1"};
+
+        Cursor cursor = contentResolver.query(uri, null, selection, selectionArgs, null);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                @SuppressLint("Range") String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                // Obține numărul de telefon
+                Cursor phoneCursor = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                        new String[]{contactId},
+                        null
+                );
+
+                if (phoneCursor != null && phoneCursor.moveToFirst()) {
+                    @SuppressLint("Range") String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    Contacts.add(new emergContact(name, phoneNumber));
+                    phoneCursor.close();
+                }
+            }
+            cursor.close();
+        }
+
+        return Contacts;
+    }
+
+    private void sendLocationAndMessage(emergContact contact){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                    if (location != null) {
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        String locationLink = "https://www.google.com/maps?q=" + latitude + "," + longitude;
+                        System.out.println(locationLink);
+                        contact.sendEmergMessage(MainActivity.this, locationLink);
+                    }
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == SMS_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission accepted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission needed", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (requestCode == LOCATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permisiune locație acordată!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permisiunea pentru locație este necesară!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ SafeAlertApp/app/src/main/java/com/example/safealertapp/emergContact.java
@@ -0,0 +1,46 @@
+package com.example.safealertapp;
+
+import android.content.Context;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+public class emergContact {
+    public String name;
+    public String phoneNumber;
+
+    public emergContact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void sendEmergMessage(Context context, String locationLink) {
+        String Message1 = "This is an automated message. This person set your contact as an emergency contact. Please look out for them, dear " + name;
+        String Message2 = "The location is: "+locationLink;
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, Message1, null, null);
+            smsManager.sendTextMessage(phoneNumber, null, Message2, null, null);
+
+            Toast.makeText(context, "Mesaj trimis la " + name, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(context, "Eroare la trimiterea mesajului", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
