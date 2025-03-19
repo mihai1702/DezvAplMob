9bed5f05ae4c9d49df09f3895a68cc4f1d1904cc
mihai1702 <apopi.mihai@gmail.com>
2025-03-19 10:42:35 +0200

    lab9

---
+++ Lab9/lab9_ex1/app/src/main/java/com/example/la9_ex1/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.la9_ex1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView batteryStatusTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batteryStatusTxt = findViewById(R.id.batteryStatusTxt);
+
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        registerReceiver(batteryReceiver, filter);
+    }
+
+    private final BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (Intent.ACTION_BATTERY_CHANGED.equals(action)) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+
+
+                if (level != -1 ) {
+                    batteryStatusTxt.setText("Battery Level: " + level + "%");
+                } else {
+                    batteryStatusTxt.setText("Battery info unavailable");
+                }
+            }
+
+            if (Intent.ACTION_BATTERY_LOW.equals(action)) {
+                batteryStatusTxt.setText("Battery is low! Charge phone now!");
+                Toast.makeText(context, "Battery is low!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(batteryReceiver);
+    }
+}
